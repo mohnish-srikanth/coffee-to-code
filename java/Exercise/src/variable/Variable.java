6e144852ab1c0895547ceba1a6c61bc709b64b5a
mohnish-srikanth <mohnishsrikanth@gmail.com>
2024-09-18 01:55:18 +0530

    java variables and hello world

---
+++ java/Exercise/src/variable/Variable.java
@@ -0,0 +1,17 @@
+package variable;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		
+		int a = 10, b = 20; // this is a more convenient declaration
+//		int b = 20;
+		int c = a + b;
+		System.out.println("Result = " + c); // this will print "Result = 30"
+		System.out.println("Result = " + a + b); // this will print "Result = 1020"
+		System.out.println("Result = " + (a + b)); // this will print "Result = 30"
+		
+		
+	}
+
+}
